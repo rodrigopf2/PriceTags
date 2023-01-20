2885feffc510aa54b235e1ebb1402ad624c95446
Petelin <rodrigopf2@hotmail.com>
2023-01-20 10:33:27 -0400

    first one

---
+++ Entities/ImportedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace price_tags.Entities
+{
+    class ImportedProduct : Product
+    {
+        public Double CustomsFee { get; set; }
+
+        public ImportedProduct(string name, double price, double customsFee)
+            : base(name, price)
+        {
+
+            CustomsFee = customsFee;
+        }
+
+        public virtual double totalPrice()
+        {
+            return Price + CustomsFee;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace price_tags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
